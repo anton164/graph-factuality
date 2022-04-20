7f5fb452dab69960daf58254f065d314e0945234
larskouwenhoven <larskouwenhoven@me.com>
2022-04-20 15:28:14 -0400

    Add streamlit app

---
+++ ui/compare_summary.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import os
+st.set_page_config(layout="wide")
+
+st.title('Comparison of Summaries')
+
+ref_loc = st.text_input("Location of reference dataset", "/Users/Lars/Downloads/refs")
+summary_1_loc = st.text_input("Location of generated summary", "/Users/Lars/Downloads/output")
+summary_2_loc = st.text_input("Location of generated summary (second)", "/Users/Lars/Downloads/output-2")
+
+all_files = os.listdir(ref_loc)
+
+file_selected = st.selectbox("Select file to compare", all_files)
+file_number = file_selected.strip(".ref")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("Reference")
+    with open(ref_loc + "/" + file_selected, 'r') as f:
+        ref_text = f.read()
+    st.write(ref_text)
+
+with col2:
+    st.header("Summary 1")
+    load_sum_1 = st.checkbox('Load summary')
+    if load_sum_1:
+        with open(summary_1_loc + "/" + file_number + ".dec", 'r') as f:
+            sum_text = f.read()
+        st.write(sum_text)
+
+with col3:
+    st.header("Summary 2")
+    load_sum_2 = st.checkbox('Load summary 2')
+    if load_sum_2:
+        with open(summary_2_loc + "/" + file_number + ".dec", 'r') as f:
+            sum_text = f.read()
+        st.write(sum_text)
